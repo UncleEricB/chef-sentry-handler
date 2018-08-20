3d6a06eaef26d203b254f0c024e01bef24274869
Mason Donahue <mason@sproutsocial.com>
2018-08-20 13:15:11 -0500

    Loosen requirement on chef_handler cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,16 @@
 name 'chef-sentry-handler'
 maintainer 'John Bellone'
 maintainer_email 'jbellone@bloomberg.net'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures a Chef handler for sending errors to Sentry.'
 long_description 'Installs and configures a Chef handler for sending errors to Sentry.'
-version '2.2.0'
-chef_version '>= 12.1'
+version '2.2.1'
+chef_version '>= 12.1' if respond_to? :chef_version
+%w(aix amazon debian centos redhat ubuntu windows).each do |os|
+  supports os
+end
 
-depends 'chef_handler', '~> 2.0'
+depends 'chef_handler', '> 2.0' # ~FC121
 
-issues_url 'https://github.com/johnbellone/chef-sentry-handler/issues'
-source_url 'https://github.com/johnbellone/chef-sentry-handler'
+issues_url 'https://github.com/johnbellone/chef-sentry-handler/issues' if respond_to? :issues_url
+source_url 'https://github.com/johnbellone/chef-sentry-handler' if respond_to? :source_url
